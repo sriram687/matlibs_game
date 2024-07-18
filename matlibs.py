be028abc039fc45d6c8f9822e88aa6c6b71ead7c
pikachu <msram2006@gmail.com>
2024-07-18 11:20:11 +0530

    Got the substring between both target "<" and ">" and assigned in words

---
+++ matlibs.py
@@ -0,0 +1,18 @@
+f = open("story.txt", "r")
+story = f.read()
+
+words = set()
+start_of_word =-1
+
+target_start = "<"
+target_end = ">"
+
+for i, char in enumerate(story):
+    if char == target_start:
+        start_of_word = i
+
+    if char ==  target_end and start_of_word != -1:
+        word = story[start_of_word: i + 1]
+        words.add(word)
+        start_of_word = -1      
+
